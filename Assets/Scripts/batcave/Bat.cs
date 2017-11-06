cb18f0b9ffc5b2c7b0677fb9ebb82f69fea83f4e
mosem <mandelmoshe@gmail.com>
2017-11-06 16:25:44 +0200

    initial build
    implemented code in bat.cs.
    functions: FixedUpdate, OnCollisionEnter2D

---
+++ Assets/Scripts/batcave/Bat.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using Infra.Gameplay.UI;
+
+namespace BatCave {
+/// <summary>
+/// The Bat controller. Responsible for playing bat animations, handling collision
+/// with the cave walls and responding to player input.
+/// </summary>
+[RequireComponent(typeof(Animator), typeof(Rigidbody2D))]
+public class Bat : MonoBehaviour {
+    [Header("Movement")]
+    [SerializeField] float flyYSpeed;
+    [SerializeField] float xSpeed;
+
+    [Header("Animation")]
+    [SerializeField] string flyUpBoolAnimParamName;
+    [SerializeField] string isAliveBoolAnimParamName;
+
+    [Header("State")]
+    [SerializeField] bool isAlive;
+
+    [Header("Testing")]
+    [SerializeField] bool isInvulnerable;
+
+    private int flyUpBoolAnimParamId;
+    private int isAliveBoolAnimParamId;
+
+    private bool flyUp;
+    private Animator animator;
+    private Rigidbody2D body;
+
+    protected void Awake() {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        flyUpBoolAnimParamId = Animator.StringToHash(flyUpBoolAnimParamName);
+        isAliveBoolAnimParamId = Animator.StringToHash(isAliveBoolAnimParamName);
+
+
+
+        GameInputCapture.OnTouchDown += OnTouchDown;
+        GameInputCapture.OnTouchUp += OnTouchUp;
+
+        isAlive = true;
+    }
+
+    protected void OnDestroy() {
+        GameInputCapture.OnTouchDown -= OnTouchDown;
+        GameInputCapture.OnTouchUp -= OnTouchUp;
+    }
+
+    protected void Update() {
+        if (!isAlive) return;
+
+        // Handle keyboard input.
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            flyUp = true;
+        } else if (Input.GetKeyUp(KeyCode.Space)) {
+            flyUp = false;
+        }
+        animator.SetBool(flyUpBoolAnimParamId, flyUp);
+        animator.SetBool(isAliveBoolAnimParamId, isAlive);
+    }
+
+    protected void FixedUpdate() {
+        if (!isAlive) return;
+        // EXERCISE: Set the bat's body.velocity so it will always move at
+        //           xSpeed to the right and at flyYSpeed if it should fly up.
+		body.velocity = flyUp ? new Vector3(xSpeed,flyYSpeed,0) : new Vector3(xSpeed,0,0);
+    }
+
+    protected void OnCollisionEnter2D(Collision2D collision) {
+        if (isInvulnerable) return;
+
+        // EXERCISE: Stop flying and die.
+        isAlive = false;
+        animator.Play("BatDie");
+
+    }
+
+    private void OnTouchDown(PointerEventData e) {
+        if (!isAlive) return;
+        flyUp = true;
+    }
+
+    private void OnTouchUp(PointerEventData e) {
+        flyUp = false;
+    }
+}
+}
